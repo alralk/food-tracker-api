bfc5cd510f23606611f9acdf5717888db68babae
alralk <alyara.alkan@gmail.com>
2026-02-13 16:55:47 +0200

    fixed format

--- dashboard.py
+++ dashboard.py
@@ -22,7 +22,7 @@ if st.session_state.token is None:
             try:
                 response = requests.post(
                     f"{API_URL}/login",
-                    data={"username": username, "password": password},
+                    json={"username": username, "password": password},
                     timeout=5
                 )
                 if response.status_code == 200:
